c1db98de78f9549e9350ae6ccf69f08f5ac17d91
Saman M <maftoun95@gmail.com>
2017-07-05 16:35:33 -0700

    one fewer errors : /

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,10 @@ var webpack = require('webpack');
 var path = require('path');
 
 var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
-var APP_DIR = path.resolve(__dirname, 'src/server');
+var APP_DIR = path.resolve(__dirname, 'src/client/app');
 
 var config = {
-    entry: APP_DIR + '/index.js',
+    entry: APP_DIR + '/App.js',
     output: {
         path: BUILD_DIR,
         filename: 'bundle.js'
